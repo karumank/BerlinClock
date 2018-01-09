9b9dce8970e426afd7f08341fe693eba65c5c1dd
karumank <karumank@mail.gvsu.edu>
2018-01-09 01:55:20 -0500

    Included Unit Test for GetClockUnitTimes method

--- BerlinClock.Tests/Controllers/HomeControllerTest.cs
+++ BerlinClock.Tests/Controllers/HomeControllerTest.cs
@@ -6,6 +6,10 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BerlinClock;
 using BerlinClock.Controllers;
+using BerlinClock.Services;
+using BerlinClock.Models;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
 
 namespace BerlinClock.Tests.Controllers
 {
@@ -26,29 +30,47 @@ namespace BerlinClock.Tests.Controllers
         }
 
         [TestMethod]
-        public void About()
+        public void GetClockUnitTimes()
         {
             // Arrange
             HomeController controller = new HomeController();
+            ClockService clockService = ClockService.Instance();
 
+            var inputTime = "05:30";
             // Act
-            ViewResult result = controller.About() as ViewResult;
+            var result = controller.GetClockUnitTimes(inputTime);
+  
+            clockService.Reset();
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Seconds", isActive = true, RowCount = 0, ColumnCount = 0, RowColor = "yellow" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = true, RowCount = 1, ColumnCount = 0, RowColor = "red" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = false, RowCount = 1, ColumnCount = 1, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = false, RowCount = 1, ColumnCount = 2, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = false, RowCount = 1, ColumnCount = 3, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = false, RowCount = 2, ColumnCount = 1, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = false, RowCount = 2, ColumnCount = 2, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = false, RowCount = 2, ColumnCount = 3, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Hours", isActive = false, RowCount = 2, ColumnCount = 4, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = true, RowCount = 3, ColumnCount = 0, RowColor = "yellow" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = true, RowCount = 3, ColumnCount = 1, RowColor = "yellow" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = true, RowCount = 3, ColumnCount = 2, RowColor = "red" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = true, RowCount = 3, ColumnCount = 3, RowColor = "yellow" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = true, RowCount = 3, ColumnCount = 4, RowColor = "yellow" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = true, RowCount = 3, ColumnCount = 5, RowColor = "red" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 3, ColumnCount = 6, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 3, ColumnCount = 7, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 3, ColumnCount = 8, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 3, ColumnCount = 9, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 3, ColumnCount = 10, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 4, ColumnCount = 1, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 4, ColumnCount = 2, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 4, ColumnCount = 3, RowColor = "NA" });
+            clockService.AddClockUnit(new ClockUnit() { TimeUnit = "Minutes", isActive = false, RowCount = 4, ColumnCount = 4, RowColor = "NA" });
 
+            var test = new JavaScriptSerializer().Serialize(result.Data);
+            var test1 = new JavaScriptSerializer().Serialize(clockService.GetAllClockUnits());
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual(test, test1);
         }
 
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
     }
 }
